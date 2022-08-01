84ef49390572ecba4cba773a8f0914bdb17c3730
xsjie <13592841305@163.com>
2022-08-01 23:19:25 +0800

    vue3 学习

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+// 程序的主入口文件
+
+// 引入createApp函数，创建对应的应用，产生应用的实例对象
 import { createApp } from 'vue'
+// 引入App组件（所有组件的父组件）
 import App from './App.vue'
+// 引入路由
 import router from './router'
+// 引入vuex
 import store from './store'
 
+
+// 创建App应用返回实例对象，注册vuex实例和路由实例对象，调用mount方法进行挂载
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// lang="ts" => 可以使用ts的代码
+
+//  defineComponent函数目的是定义一个组件，内部可以传入一个配置对象。使用配置对象和vue2是一样的用法
+
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
